4c3aadbb3b86f1d1ea04bb6eb640a20a68cc2aaa
Gustavo Souza <gustavobat.gb@gmail.com>
2025-04-23 15:22:19 +0000

    small localization changes and trying to locate a image in the page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: 'pt-br',
+    locales: ['en', 'pt-br'],
     routing: {
       prefixDefaultLocale: false
     }
